b48638c5c579d277ec4c4b6ca35b6d72eacde851
Elan Colello <elanishere@gmail.com>
2017-03-08 13:41:27 -0700

    new app

---
+++ festival/Assets/Vuforia/Scripts/FocusModeCAuto.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+namespace Vuforia
+{
+	/// <summary>
+	/// Helper behaviour used to hide augmented objects behind the video background.
+	/// </summary>
+	public class FocusModeCAuto : MonoBehaviour{
+
+		void Start ()
+		{
+
+
+			bool focusModeSet = CameraDevice.Instance.SetFocusMode (
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+		}
+
+	}
+
+}
